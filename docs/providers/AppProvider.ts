1dcd328030e6d625ba286c40a617547a2d310410
Josh Manders <josh@joshmanders.com>
2023-10-10 18:52:28 -0500

    remove old code

--- docs/providers/AppProvider.ts
+++ docs/providers/AppProvider.ts
@@ -1,9 +1,3 @@
-import clsx from 'clsx';
-import { addCollection, edgeIconify } from 'edge-iconify';
-import { twMerge } from 'tailwind-merge';
-
-import { icons as tablerIcons } from '@iconify-json/tabler';
-
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application';
 
 export default class AppProvider {
@@ -19,14 +13,6 @@ export default class AppProvider {
 
   public async ready() {
     // App is ready
-    const View = this.app.container.use('Adonis/Core/View');
-
-    addCollection(tablerIcons);
-    View.use(edgeIconify);
-
-    View.global('clsx', (...args: any[]) => {
-      return twMerge(clsx(...args));
-    });
   }
 
   public async shutdown() {
