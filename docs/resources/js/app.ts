7953082eeb046e6d2f0973a13d35812ed6777527
Josh Manders <josh@joshmanders.com>
2023-10-11 23:07:15 -0500

    start working on dark mode

--- docs/resources/js/app.ts
+++ docs/resources/js/app.ts
@@ -1,14 +1,38 @@
 import 'tailwindcss/tailwind.css';
 
+import Alpine from 'alpinejs';
 import htmx from 'htmx.org';
 
 declare global {
   interface Window {
+    Alpine: typeof Alpine;
     htmx: typeof htmx;
+    toggleDarkMode: () => void;
   }
 }
 
+window.Alpine = Alpine;
 window.htmx = htmx;
+
+Alpine.start();
+
+window.toggleDarkMode = () => {
+  const html = document.querySelector('html');
+  const lightToggle = document.querySelector('#light-toggle');
+  const darkToggle = document.querySelector('#dark-toggle');
+
+  if (html!.classList.contains('dark')) {
+    lightToggle!.classList.remove('hidden');
+    darkToggle!.classList.add('hidden');
+    html!.classList.remove('dark');
+    html!.classList.add('light');
+  } else {
+    lightToggle!.classList.add('hidden');
+    darkToggle!.classList.remove('hidden');
+    html!.classList.remove('light');
+    html!.classList.add('dark');
+  }
+};
 
 require('htmx.org/dist/ext/head-support');
 require('htmx-ext/dist/body-support');
