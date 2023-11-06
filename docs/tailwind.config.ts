4cd1b600181bc652b0bf207ca0f337b1d1616510
Josh Manders <josh@joshmanders.com>
2023-11-06 16:23:12 -0600

    alias primary to fuschia

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -1,10 +1,18 @@
+import colors from 'tailwindcss/colors';
+
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: 'class',
   content: ['./resources/views/**/*.edge', '../node_modules/nifty-ui/ui/components/**/*.edge'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('tailwind-htmx')],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.fuchsia,
+      },
+    },
+  },
 };
 
 export default config;
