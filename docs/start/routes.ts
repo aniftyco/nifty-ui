55e93d1d3f377b5eeb0e58e593424a4c291fe879
Josh Manders <josh@joshmanders.com>
2023-11-07 00:11:39 -0600

    add rough support for markdown rendering of pages

--- docs/start/routes.ts
+++ docs/start/routes.ts
@@ -18,6 +18,73 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route';
+import { readFileSync } from 'fs';
+import { plugin as tableOfContent } from 'markdown-toc';
+import { join } from 'path';
+import { Remarkable } from 'remarkable';
+import meta from 'remarkable-meta';
+import { linkify } from 'remarkable/linkify';
 
-Route.on('/').render('docs').as('docs');
+import Route from '@ioc:Adonis/Core/Route';
+import { string } from '@poppinss/utils/build/helpers';
+
+import type { HeadingOpenToken, HeadingCloseToken, ContentToken } from 'remarkable/lib';
+
+const headerIdLinks = (remarkable: Remarkable) => {
+  const levels = [2, 3];
+  const open = remarkable.renderer.rules.heading_open;
+  const close = remarkable.renderer.rules.heading_close;
+
+  remarkable.renderer.rules.heading_open = (tokens: HeadingOpenToken[], idx: number) => {
+    const level = tokens[idx].hLevel;
+    const content = (tokens[idx + 1] as ContentToken).content;
+    const slug = string.dashCase(content);
+
+    if (levels.indexOf(level) !== -1) {
+      return `<h${level} id="${slug}" class="doc-heading group">`;
+    }
+
+    return open(tokens, idx);
+  };
+
+  remarkable.renderer.rules.heading_close = (tokens: HeadingCloseToken[], idx: number) => {
+    const level = tokens[idx].hLevel;
+    const content = (tokens[idx - 1] as ContentToken).content;
+    const slug = string.dashCase(content);
+
+    if (levels.indexOf(level) !== -1) {
+      return `<a href="#${slug}">#</a></h${level}>`;
+    }
+
+    return close(tokens, idx);
+  };
+};
+
+const remarkable = new Remarkable({ html: true, typographer: true });
+remarkable.use(meta).use(headerIdLinks).use(linkify);
+
+Route.on('/').render('landing').as('landing');
+Route.on('/docs').redirect('docs.page', { section: 'introduction' }).as('docs.index');
+Route.get('/docs/:section/:page?', async ({ params, view }) => {
+  const page = params.page ? `${params.section}/${params.page}` : params.section;
+  const markdown = readFileSync(join(__dirname, '../content', `${page}.md`), 'utf8');
+  const content = remarkable.render(markdown);
+  const { json } = (new Remarkable() as any)
+    .use(tableOfContent({ firsth1: false, stripHeadingTags: true }))
+    .render(markdown);
+
+  // Generate table of content from headings
+  const toc: any[] = [];
+
+  json.slice(1).forEach((item: { lvl: 2 | 3; content: string; slug: string }) => {
+    if (item.lvl === 2) {
+      toc.push({ anchor: item.content, slug: item.slug, items: [] });
+    }
+
+    if (item.lvl === 3) {
+      toc[toc.length - 1].items.push({ anchor: item.content, slug: item.slug });
+    }
+  });
+
+  return view.render('docs', { meta: (remarkable as any).meta, toc, content });
+}).as('docs.page');
