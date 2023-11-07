d5fa1fdec581d70d73976743032ba184aa729f6f
Josh Manders <josh@joshmanders.com>
2023-11-07 09:17:59 -0600

    add to safelist

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -5,6 +5,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: 'class',
   content: ['./resources/views/**/*.edge', '../node_modules/nifty-ui/ui/components/**/*.edge'],
+  safelist: ['doc-heading'],
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('tailwind-htmx')],
   theme: {
     extend: {
